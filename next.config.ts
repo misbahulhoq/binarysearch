1d6fee5a0ed927a5dfbda8a46ed954b8778c8275
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-03-17 21:15:05 +0600

    switching to dev branch after installing nextra

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
+import nextra from "nextra";
+const withNextra = nextra({
+  // ... Other Nextra config options
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default withNextra({
+  // ... Other Next.js config options
+});
